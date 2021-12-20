e315be15f020d5a2b4b8fda2d147ea9126fe8ca6
WickramBug <wickram95@gmail.com>
2021-12-20 13:41:56 +0530

    Add modified state file writer

--- acr-write-state-file.sh
+++ acr-write-state-file.sh
@@ -1,1 +1,45 @@
-echo "test" -> test.txt
+# Declare variables
+PRESENT_TAGS=()
+FILE="acr-image-state.txt"
+REGISTRY=wickramContainerRegistry001
+REPOSITORY=hi_mom_nginx
+DIGEST="sha256:5ef43aba3beef6f73965d6ce357e041af0174e3739ae110693d4c363cf2028ac"
+
+# Get the production image tag
+image_tag=$(az acr repository show-manifests --name $REGISTRY --repository $REPOSITORY \
+        -o tsv --query "[?digest == '$DIGEST'].[tags[0]]")
+
+# Create file if not exist
+if [ ! -f "$FILE" ]; then
+    >"$FILE"
+fi
+
+# Read state file to an array
+readarray -t PRESENT_TAGS <"$FILE"
+
+# Get array size
+PRESENT_TAGS_SIZE=${#PRESENT_TAGS[@]}
+
+# Check if image tag is already available
+if [[ "${PRESENT_TAGS[*]}" == *"$image_tag"* ]]; then
+    # Exit if image tag found
+    exit
+else
+    # Check array size
+    if [[ "$PRESENT_TAGS_SIZE" < 5 ]]; then
+        # Add new image tag if array size is -le to five
+        PRESENT_TAGS+=("$image_tag")
+        # Empty state file
+        >"$FILE"
+        # Update state file with new tag lists
+        printf "%s\n" ${PRESENT_TAGS[@]} >>acr-image-state.txt
+    else
+        # Remove oldest image from the state file if array size is -gt five
+        PRESENT_TAGS=("${PRESENT_TAGS[@]:1}")
+        # Add new image tag to array
+        PRESENT_TAGS+=("$image_tag")
+        >"$FILE"
+        # Update state file with new tag lists
+        printf "%s\n" ${PRESENT_TAGS[@]} >>acr-image-state.txt
+    fi
+fi
