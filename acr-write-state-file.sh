9ff5148e32810e40aa32958947cd94124b9341e9
WickramBug <wickram95@gmail.com>
2021-12-20 11:24:20 +0530

    Modify acr state write

--- acr-write-state-file.sh
+++ acr-write-state-file.sh
@@ -1,6 +1,8 @@
 echo "test" -> test.txt
 
+git config --global user.email "wickram95@gmail.com"
+git config --global user.name "WickramBug"
 git status
-git add text.txt
+git add test.txt
 git commit -m "Update state file"
 git push origin main 
