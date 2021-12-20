599fb695b073fc7f9e7b2689338702d80810908d
WickramBug <wickram95@gmail.com>
2021-12-20 11:36:00 +0530

    Remove git push

--- acr-write-state-file.sh
+++ acr-write-state-file.sh
@@ -1,8 +1,1 @@
 echo "test" -> test.txt
-
-git config --global user.email "wickram95@gmail.com"
-git config --global user.name "WickramBug"
-git status
-git add test.txt
-git commit -m "Update state file"
-git push origin main 
