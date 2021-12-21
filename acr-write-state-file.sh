f92c11f45613121f57c5c6bad643e4f812a54a23
WickramBug <wickram95@gmail.com>
2021-12-21 12:09:44 +0530

    Update digest

--- acr-write-state-file.sh
+++ acr-write-state-file.sh
@@ -3,7 +3,7 @@ PRESENT_TAGS=()
 FILE="acr-image-state.txt"
 REGISTRY=wickramContainerRegistry001
 REPOSITORY=hi_mom_nginx
-DIGEST="sha256:3023ae84a4e7239d819bd96609e89d65bdf7a7b2b9fdb5c4132f6e64a269f1a4"
+DIGEST="sha256:a3bc946b11e888fed558dfc432ade6c626c750de52d765f32f3d1f41e2919bb0"
 
 # Get the production image tag
 image_tag=$(az acr repository show-manifests --name $REGISTRY --repository $REPOSITORY \
